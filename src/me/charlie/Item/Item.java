a57b484ab3ff68a9ce4dabf7d06fa2c697352881
Abthec <40231875+Abthec@users.noreply.github.com>
2021-05-03 20:19:37 +1200

    Assigned basic store inventories and item costs.

--- src/me/charlie/Item/Item.java
+++ src/me/charlie/Item/Item.java
@@ -1,15 +1,34 @@
 package me.charlie.Item;
 
+import java.util.Random;
+
 public class Item {
 
     ItemType itemType;
     int size;
+    int cost;
     String description;
+
+    Random random = new Random();
 
     public Item(ItemType itemType) {
         this.itemType = itemType;
         this.size = itemType.getSize();
         this.description = itemType.getDescription();
+        switch (this.itemType) {
+            case ARTIFACT:
+                this.cost = 750 + random.nextInt(50);
+            case WEAPON:
+                this.cost = 75 + random.nextInt(15);
+            case JEWEL:
+                this.cost = 300 + random.nextInt(50);
+            case UPGRADE:
+                this.cost = 175 + random.nextInt(25);
+        }
+    }
+
+    public int getCost() {
+        return cost;
     }
 
     public int getSize() {
--- src/me/charlie/Store/Store.java
+++ src/me/charlie/Store/Store.java
@@ -1,13 +1,33 @@
 package me.charlie.Store;
 
+import java.util.Random;
+
 public class Store {
 
     StoreType storeType;
-    double buyModifier;
-    double sellModifier;
     int inventorySize;
 
-    public Store(double buyModifier, double sellModifier, StoreType storeType, int inventorySize) {
+    private Random random = new Random();
 
+    public Store(StoreType storeType) {
+        this.storeType = storeType;
+        switch (this.storeType) {
+            case BLACKSMITH:
+                this.inventorySize = 30 + random.nextInt(21);
+            case SHIP_BUILDER:
+                this.inventorySize = 20 + random.nextInt(21);
+            case JEWELER:
+                this.inventorySize = 40 + random.nextInt(21);
+            case ARTIFICER:
+                this.inventorySize = 15 + random.nextInt(26);
+        }
+    }
+
+    public int getInventorySize() {
+        return inventorySize;
+    }
+
+    public StoreType getStoreType() {
+        return storeType;
     }
 }
