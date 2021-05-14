d24154fec48c1bed1c1548ca44c652eea496b97d
Abthec <40231875+Abthec@users.noreply.github.com>
2021-05-15 10:46:26 +1200

    GUI: added classes to represent first two screens

---
+++ src/me/charlie/Gui/ShipSelector.java
@@ -0,0 +1,43 @@
+package me.charlie.Gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ShipSelector {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ShipSelector window = new ShipSelector();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ShipSelector() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
---
+++ src/me/charlie/Gui/StartupScreen.java
@@ -0,0 +1,43 @@
+package me.charlie.Gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class StartupScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StartupScreen window = new StartupScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public StartupScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
