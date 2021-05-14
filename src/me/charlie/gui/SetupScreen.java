e812c24e16bbed5ab76a38ac200d99fc5fab2209
Abthec <40231875+Abthec@users.noreply.github.com>
2021-05-15 10:52:24 +1200

    Created setup screen

---
+++ src/me/charlie/gui/SetupScreen.java
@@ -0,0 +1,43 @@
+package me.charlie.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class SetupScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SetupScreen window = new SetupScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public SetupScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
