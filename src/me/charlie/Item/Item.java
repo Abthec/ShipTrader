3b3d05072793d09549f4c8d7dc3557dadb04fd00
Abthec <40231875+Abthec@users.noreply.github.com>
2021-05-26 12:58:50 +1200

    Item javadocs.

--- src/me/charlie/Item/Item.java
+++ src/me/charlie/Item/Item.java
@@ -41,75 +41,151 @@ public class Item {
         }
     }
     
+    /**
+     * Sets the boolean usedAsUpgrade to true.
+     */
     public void setUsedAsUpgrade() {
     	this.usedAsUpgrade = true;
     }
     
+    /**
+     * 
+     * @return if the Item was used as an Upgrade or not.
+     */
     public boolean usedAsUpgrade() {
     	return usedAsUpgrade;
     }
     
+    /**
+     * Assigns the value to where the item was sold.
+     * 
+     * @param island the place the Item was sold.
+     */
     public void setPlaceOfSale(Island island) {
     	placeOfSale = island;
     }
     
+    /**
+     * 
+     * @return the Island where the item was sold.
+     */
     public Island getPlaceOfSale() {
     	return placeOfSale;
     }
     
+    /**
+     * Assigns the price that the Item was sold for.
+     * 
+     * @param price the price the Item was sold for.
+     */
     public void setSoldPrice(int price) {
     	soldPrice = price;
     }
     
+    /**
+     * 
+     * @return the price the Item was sold for.
+     */
     public int getSoldPrice() {
     	return soldPrice;
     }
     
+    /**
+     * Assign the Island the Item was purchased on.
+     * 
+     * @param island where the Item was purchased.
+     */
     public void setPlaceOfPurchase(Island island) {
     	this.placeOfPurchase = island;
     }
     
+    /**
+     * 
+     * @return the place the Item was purchased.
+     */
     public Island getPlaceOfPurchase() {
     	return placeOfPurchase;
     }
     
+    /**
+     * Assigns the value the Item was purchased for.
+     * 
+     * @param purchasedPrice the price the Item was purchased for.
+     */
     public void setPurchasedPrice(int purchasedPrice) {
     	this.purchasedPrice = purchasedPrice;
     }
     
+    /**
+     * 
+     * @return the price the Item was purchased for.
+     */
     public int getPurchasedPrice() {
     	return purchasedPrice;
     }
 
+    /**
+     * 
+     * @return the Item's cost before buy or sell modifiers are applied.
+     */
     public int getBaseCost() {
         return baseCost;
     }
 
+    /**
+     * 
+     * @return the Item's cost after a buy modifier has been applied.
+     */
     public int getBuyCost() {
         return buyCost;
     }
 
+    /**
+     * Calculates the Item's sell cost given a specific Store.
+     * 
+     * @param store the Store the Item is being sold at.
+     * @return the cost of the Item based off the Store it's being sold at.
+     */
     public int getSellCost(Store store) {
         sellCost = (int)Math.round(this.getBaseCost() * store.getStoreSellModifier(this));
         return sellCost;
     }
 
+    /**
+     * 
+     * @return how much cargo space the Item takes up.
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * 
+     * @return the Item's description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * 
+     * @return the Item's ItemType.
+     */
     public ItemType getItemType() {
         return itemType;
     }
 
+    /**
+     * 
+     * @return the Item's UpgradeType.
+     */
     public UpgradeType getUpgradeType() {
         return upgradeType;
     }
 
+    /**
+     * Converts an Item object to a String.
+     */
     public String toString() {
         return this.getItemType().getName() + " | ";
     }
